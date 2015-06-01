ec88de289aba66a8332a1ff01d507a378aee889b
lizhiqianduan <1024669228@qq.com>
2015-06-01 18:14:45 +0800

    'is-ok'

--- src/lz-pager.js
+++ src/lz-pager.js
@@ -4,87 +4,61 @@ create by xiaohei on 2015/6/1
 */
 
 
+
+
 /*
-html结构：
-<div class="lz-pager">
+ * 获取分页的按钮
+ * object vm
+ * @totalNum : 总条数
+ * @pageSize : 页面的条数
+ * @offset : 分页按钮的偏移量
+ * @pageOnClickFn : 分页点击函数，默认传递参数page --> 页号
+ * */
+function lzPager(vm){
+//            处理分页
+    var totalItem = vm.totalNum?vm.totalNum:0;
+    var pageSize = vm.pageSize ? vm.pageSize : 20;
+    var offset = vm.offset ? vm.offset : 5;
+    var page = vm.curPage ? vm.curPage : 1;
+    var pageOnClickFn = vm.pageOnClickFn ? vm.pageOnClickFn : "alert";
+    if(totalItem == 0 || !totalItem){
+        return "";
+    }
 
-</div>
+    var pageHtml = '';
+    var pageNum = Math.ceil(totalItem/pageSize);
+    pageHtml+= "<li><a href='javascript:;'>总共" + pageNum +"页 "+ totalItem +"条</a></li>";
+    pageHtml+= "<li><a href='javascript:;' onclick='"+ pageOnClickFn +"(1)'>首页</a></li>";
+    if(page != 1){
+        pageHtml+= "<li><a href='javascript:;' onclick='"+ pageOnClickFn +"("+ (page-1) +")'>上一页</a></li>";
+    }
+    if(pageNum<=2*offset){
+        for(var p=1;p<=pageNum;p++){
+            pageHtml+="<li class='"+ (p==page?'active':'') +"'><a href='javascript:;' onclick='"+ pageOnClickFn +"("+p+")'>" + p +"</li></a>";
+        }
+    }else{
+        if(page>offset && page<pageNum-offset){
+            for(var p=page-offset;p<=page+offset;p++){
+                pageHtml+="<li class='"+ (p==page?'active':'') +"'><a  href='javascript:;' onclick='"+ pageOnClickFn +"("+p+")'>" + p +"</li></a>";
+            }
+        }else if(page<=offset){
+            for(var p=1;p<=2*offset;p++){
+                pageHtml+="<li class='"+ (p==page?'active':'') +"'><a  href='javascript:;' onclick='"+ pageOnClickFn +"("+p+")'>" + p +"</li></a>";
+            }
+        }else if(page>=pageNum-offset){
+            for(var p=page-2*offset;p<=pageNum;p++){
+                pageHtml+="<li class='"+ (p==page?'active':'') +"'><a  href='javascript:;' onclick='"+ pageOnClickFn +"("+p+")'>" + p +"</li></a>";
+            }
+        }
+    }
+    if(page !=pageNum){
+        pageHtml+= "<li><a href='javascript:;' onclick='"+ pageOnClickFn +"("+ (page+1) +")'>下一页</a></li>";
+    }
 
-*/
+    pageHtml+= "<li><a href='javascript:;' onclick='"+ pageOnClickFn +"("+ pageNum +")'>尾页</a></li>";
 
-;(function($){
-
-	// define some globle function 
-	// can't be used in window,but can be used in my plugin anywhere
-	function isString(str) {
-		return typeof(str) === "string";
-	}
-
-	function isNum(str) {
-		if (typeof(str) === "number")
-			return true;
-		else
-			return false;
-	}
-
-	function isObj(str) {
-		if (typeof(str) === "object")
-			return true;
-		else
-			return false;
-	}
-
-	function isFn(str) {
-		if (typeof(str) == "function")
-			return true;
-		else
-			return false;
-	}
-
-	//合并两个对象
-	function extend(obj1, obj2) {
-		var newobj = {};
-		for (var i in obj1) {
-			newobj[i] = obj1[i];
-			if (obj2 && obj2[i] != null) {
-				newobj[i] = obj2[i];
-			}
-		}
-		return newobj;
-	}
-
-
-//  插件名lzPager
-// 需要后台返回json数组
-	$.fn.lzPager = function(options){
-		var defaultSettings = {
-			
-			// json数组中需要保留的字段
-			keepFields:[
-				"id",
-				"name",
-				"haha"
-			],
-			// 
-			userFields:[
-				""
-			],
-//            请求数据的方式：get、post
-            method:'post',
-//            请求数据的路径
-            url:'test.json'
-		};
-
-        var settings = extend(defaultSettings,options);
-        $.ajax(url,{
-            method:settings.method
-
-        });
-		
-	}
-
-})($);
-
-
+    console.log(pageHtml);
+    return pageHtml;
+}
 
 
